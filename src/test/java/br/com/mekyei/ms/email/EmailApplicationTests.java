047a171e4b2379e03a20ce3beda70121acf641e4
Mekylei-Belchior <mekylei@msn.com>
2021-08-12 14:38:35 -0300

    Estrutura do projeto

---
+++ src/test/java/br/com/mekyei/ms/email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mekyei.ms.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
